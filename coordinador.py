75f7e088f8fd3cbb1e5f3d05108344a6cd9f5bc1
nombreUsuario <eder2210051@correo.uis.edu.co>
2024-02-15 18:00:45 -0500

    cambios de clase

--- coordinador.py
+++ coordinador.py
@@ -7,4 +7,9 @@ def registrarNota():
         nombre = camper.get('nombre','No hay nombre')
         apellido = camper.get('apellido','No hay apellido')
         print(f"{index+1} - {nombre} {apellido}")
-    
+    numEstudiante = int(input("Digita el numero del camper que deseas registrarle nota: "))
+    nota1 = int(input("Ingresa el valor de la primera nota: "))
+    nota2 = int(input("Ingresa el valor de la segunda nota: "))
+    promedio = (nota1+nota2)/2
+    if(promedio>=60):
+        listaCamper[numEstudiante-1]['estado']="aprobado"
